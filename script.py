201835cb1fee1bafe9d7efc9701cc608717e69c3
Swaroop <141357728+swar41@users.noreply.github.com>
2024-01-14 21:08:57 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,14 +3,20 @@ from selenium import webdriver
 from selenium.webdriver.common.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import pytest
 
 # Set up the WebDriver (ma
 # ke sure you have the appropriate browser driver installed)
-driver = webdriver.Chrome()  # You can use other browser drivers like Firefox, Edge, etc.
+@pytest.fixture(autouse=True, scope='class')
+def setup(request):
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)# You can use other browser drivers like Firefox, Edge, etc.
 
     # Open the web pageC:C:\Users\swaro\OneDrive\Desktop\PD\selen\index.html
-driver.get("http://localhost:8000/selen")  # Replace with the actual path
+driver.get("http://localhost:8080/")  # Replace with the actual path
 
 title = driver.title
 if title == 'Enabler':
