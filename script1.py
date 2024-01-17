9363ef6f93c9d70c476eb15e0020fd632e8fa322
Swaroop <141357728+swar41@users.noreply.github.com>
2024-01-17 18:27:15 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -3,11 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 import pytest
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))# You can use other browser drivers like Firefox, Edge, etc.
+driver = webdriver.Chrome()# You can use other browser drivers like Firefox, Edge, etc.
 
     # Open the web pageC:C:\Users\swaro\OneDrive\Desktop\PD\selen\index.html
 driver.get("http://localhost:8080/")  # Replace with the actual path
