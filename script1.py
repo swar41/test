950cee50ae1f0c04ad4b566f169990f4deac17a6
Swaroop <141357728+swar41@users.noreply.github.com>
2024-01-18 01:24:50 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -2,13 +2,7 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
-
-class TestLoginPage:
-    def test_valid_login(self, driver):
-        driver.get("http://localhost:8000/")
-        # time.sleep(2)
-
-         actual_url = driver.current_url
-        assert actual_url == "http://localhost:8000/"
-
-        # time.sleep(2)
+  driver.get("http://localhost:8000/")
+ actual_url = driver.current_url
+    assert actual_url == "http://localhost:8000/"
+    
