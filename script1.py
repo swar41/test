d531df8eb45434e8f9b2ecc5d70db97235b7877a
Swaroop <141357728+swar41@users.noreply.github.com>
2024-01-18 01:45:09 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -2,12 +2,15 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("http://localhost:8000")
-title = driver.title
+class title:
+  def tit(self,driver):    
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:8000")
+    title = driver.title
 
 # Check if the title is as expected
-expected_title = "Your Web App - Home"
-assert title == expected_title, f"Title mismatch. Expected: {expected_title}, Actual: {title}"
+    expected_title = "Your Web App - Home"
+    assert title == expected_title, f"Title mismatch. Expected: {expected_title}, Actual: {title}"
+         
 
 driver.quit()
