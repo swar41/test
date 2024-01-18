41cf1459b9162c6999ea037976f650e724591377
Swaroop <141357728+swar41@users.noreply.github.com>
2024-01-18 20:00:40 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -2,8 +2,51 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
-class title:
-  def test11(self,driver):    
-    driver.get("https://www.saucedemo.com/")
-    actual_url = driver.current_url
-    assert actual_url == "https://www.saucedemo.com/"
+class TestLoginPage:
+    def test_valid_login(self, driver):
+        driver.get("https://www.saucedemo.com/")
+        # time.sleep(2)
+
+        # Type username
+        username_input = driver.find_element(By.ID, "user-name")
+        username_input.send_keys("standard_user")
+
+        # Type password
+        password_input = driver.find_element(By.ID, "password")
+        password_input.send_keys("secret_sauce")
+
+        # Click on the login button
+        login_btn = driver.find_element(By.ID, "login-button")
+        login_btn.click()
+
+        # URL Validation
+        actual_url = driver.current_url
+        assert actual_url == "https://www.saucedemo.com/inventory.htmls"
+
+        # time.sleep(2)
+
+    @pytest.mark.parametrize("username, password, error", [
+        ("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."),
+        ("invalidUser", "invalidPass", "Epic sadface: Username and password do not match any user in this service")])
+    def test_invalid_login(self, driver, username, password, error):
+        driver.get("https://www.saucedemo.com/")
+        # time.sleep(2)
+
+        # Type username
+        username_input = driver.find_element(By.ID, "user-name")
+        username_input.send_keys(username)
+
+        # Type password
+        password_input = driver.find_element(By.ID, "password")
+        password_input.send_keys(password)
+
+        # Click on the login button
+        login_btn = driver.find_element(By.ID, "login-button")
+        login_btn.click()
+
+        # Error message validation
+        error_msg_h3 = driver.find_element(By.TAG_NAME, "h3")
+        error_msg_text = error_msg_h3.text
+        assert error_msg_text == error
+
+        # time.sleep(2)
