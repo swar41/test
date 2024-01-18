33394bc9209e2044184d765c7ed337ddeb7d944b
Swaroop <141357728+swar41@users.noreply.github.com>
2024-01-18 19:33:03 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -6,7 +6,7 @@ class title:
   def test11(self,driver):    
     driver.get("http://localhost:8000")
     title = driver.title
-     print(title)
+    print(title)
 # Check if the title is as expected
     expected_title = "Your Web App - Home"
     assert title == expected_title, f"Title mismatch. Expected: {expected_title}, Actual: {title}"
